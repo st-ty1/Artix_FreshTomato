442dc8cf8f4c622b2be648729824c9d97581946c
st-ty1 <stephan.tratzky@gmail.com>
2020-12-20 17:55:13 +0100

    Update build_ft-mips-RT-AC.sh

--- dockerfiles/build_ft-mips-RT-AC.sh
+++ dockerfiles/build_ft-mips-RT-AC.sh
@@ -15,6 +15,7 @@ git checkout mips-RT-AC
 patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src/router/common.mak
 patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Makefile
 patch -i $FT_PATCHES_DIR/mksquashfs.c.patch $FT_REPO_DIR/release/src-rt-6.x/linux/linux-2.6/scripts/squashfs/mksquashfs.c
+patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
 
 cd release/src-rt-6.x
 
