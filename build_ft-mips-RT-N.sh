a2a1220a8eb3f49bd3074822f9c3d14a784e022e
st-ty1 <stephan.tratzky@gmail.com>
2020-12-20 17:45:52 +0100

    Update build_ft-mips-RT-N.sh

--- build_ft-mips-RT-N.sh
+++ build_ft-mips-RT-N.sh
@@ -14,11 +14,8 @@ git checkout mips-RT-AC
 
 patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src/router/common.mak
 patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Makefile
-patch -i $FT_PATCHES_DIR/configure.ac_tor.patch $FT_REPO_DIR/release/src/router/tor/configure.ac
 patch -i $FT_PATCHES_DIR/mksquashfs.c.patch $FT_REPO_DIR/release/src-rt/linux/linux-2.6/scripts/squashfs/mksquashfs.c
 patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
-
-rm -f $FT_REPO_DIR/release/src/router/nettle/desdata.stamp
 
 cd release/src-rt 
 
