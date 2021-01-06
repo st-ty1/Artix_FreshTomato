a99a744414dbb1bafb1b3071027c7a217211d65d
st-ty1 <stephan.tratzky@gmail.com>
2021-01-06 19:58:27 +0100

    Update build_ft-mips-RT.sh

--- build_ft-mips-RT.sh
+++ build_ft-mips-RT.sh
@@ -3,16 +3,13 @@
 FT_PATCHES_DIR=$HOME/Artix_FreshTomato
 FT_REPO_DIR=$HOME/freshtomato-mips
 
-PATH="$FT_REPO_DIR/tools/brcm/hndtools-mipsel-uclibc/bin:$PATH"
-
-clear
-
 cd $FT_REPO_DIR 
 git clean -dxf 
 git reset --hard
 git pull
 
 git checkout mips-master
+clear
 
 patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src/router/common.mak
 patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Makefile
