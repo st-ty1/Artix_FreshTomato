a9773d863d372b05341df65b3ae850e81d33bb94
st-ty1 <stephan.tratzky@gmail.com>
2021-01-14 18:35:35 +0100

    Update build_ft-mips-RT.sh

--- build_ft-mips-RT.sh
+++ build_ft-mips-RT.sh
@@ -8,7 +8,7 @@ PATH="$FT_REPO_DIR/tools/brcm/hndtools-mipsel-uclibc/bin:$PATH"
 cd $FT_REPO_DIR 
 git clean -dxf 
 git reset --hard
-git pull
+#git pull
 
 git checkout mips-master
 clear
@@ -17,6 +17,8 @@ patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src/router/common
 patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Makefile
 patch -i $FT_PATCHES_DIR/mksquashfs.c.patch $FT_REPO_DIR/release/src-rt/linux/linux-2.6/scripts/squashfs/mksquashfs.c 
 patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
+patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src/router/apcupsd/autoconf/configure.in
+patch -i $FT_PATCHES_DIR/std-gnu11.m4.patch $FT_REPO_DIR/release/src/router/nano/m4/std-gnu11.m4
 
 cd release/src-rt 
 
