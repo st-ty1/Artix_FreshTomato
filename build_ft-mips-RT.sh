e48da378195493126267e778351fec0afe980cd9
st-ty1 <stephan.tratzky@gmail.com>
2021-01-25 11:15:27 +0100

    Update build_ft-mips-RT.sh

--- build_ft-mips-RT.sh
+++ build_ft-mips-RT.sh
@@ -18,7 +18,6 @@ patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Mak
 patch -i $FT_PATCHES_DIR/mksquashfs.c.patch $FT_REPO_DIR/release/src-rt/linux/linux-2.6/scripts/squashfs/mksquashfs.c 
 patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
 patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src/router/apcupsd/autoconf/configure.in
-patch -i $FT_PATCHES_DIR/std-gnu11.m4.patch $FT_REPO_DIR/release/src/router/nano/m4/std-gnu11.m4
 
 cd release/src-rt 
 
