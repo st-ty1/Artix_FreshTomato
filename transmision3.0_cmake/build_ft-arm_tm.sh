5fe9d4d4c42c738e8d4e4e4acf8788e75d6e8c29
st-ty1 <stephan.tratzky@gmail.com>
2021-02-08 18:01:33 +0100

    Add files via upload

---
+++ transmision3.0_cmake/build_ft-arm_tm.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+FT_PATCHES_DIR=$HOME/Artix_FreshTomato
+FT_REPO_DIR=$HOME/freshtomato-arm
+
+cd $FT_REPO_DIR 
+git clean -dxf 
+git reset --hard
+#git pull
+
+git checkout arm-master
+
+clear
+
+patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/common.mak
+patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
+patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/miniupnpd/configure
+patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/apcupsd/autoconf/configure.in
+
+cp -v $FT_PATCHES_DIR/transmission3.0_cmake/subprocess-test.cmd $FT_REPO_DIR/release/src-rt-6.x.4708/router/transmission/libtransmission
+patch -i $FT_PATCHES_DIR/transmission3.0_cmake/transmission_cmake.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
+
+cd release/src-rt-6.x.4708
+
+time make ac68z #1> log.txt 2>&1
+## AIO:z; VPN:e
