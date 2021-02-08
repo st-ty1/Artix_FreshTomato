786a72b54eb672ef6f089d61c57592a411b7ecdb
st-ty1 <stephan.tratzky@gmail.com>
2021-02-08 18:08:34 +0100

    Add files via upload

--- transmision3.0_cmake/build_ft-arm_tm.sh
+++ transmision3.0_cmake/build_ft-arm_tm.sh
@@ -12,11 +12,14 @@ git checkout arm-master
 
 clear
 
+# common patches needed by Artix/Arch Linux
 patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/common.mak
 patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/miniupnpd/configure
 patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/apcupsd/autoconf/configure.in
+patch -i $FT_PATCHES_DIR/configure.in_mysql.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/mysql/configure.in
 
+# patches specific for building transmission by cmake
 cp -v $FT_PATCHES_DIR/transmission3.0_cmake/subprocess-test.cmd $FT_REPO_DIR/release/src-rt-6.x.4708/router/transmission/libtransmission
 patch -i $FT_PATCHES_DIR/transmission3.0_cmake/transmission_cmake.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 
