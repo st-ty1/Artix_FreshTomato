6a9ecf73a9bb5b74f0e0a1eb9d2917c010d98223
st-ty1 <stephan.tratzky@gmail.com>
2021-03-27 15:18:59 +0100

    Update build_ft-arm.sh

--- build_ft-arm.sh
+++ build_ft-arm.sh
@@ -16,6 +16,7 @@ patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src-rt-6.x.4708/r
 patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/miniupnpd/configure
 patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/apcupsd/autoconf/configure.in
+patch -i  $FT_PATCHES_DIR/configure.ac_transmission.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/transmission/configure.ac
 
 cd release/src-rt-6.x.4708
 
