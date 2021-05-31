b46dfecb9669ad55a87fe1f1e2178844aa47446d
st-ty1 <stephan.tratzky@gmail.com>
2021-05-31 18:44:20 +0200

    Update build_ft-mips-RT-AC.sh

--- build_ft-mips-RT-AC.sh
+++ build_ft-mips-RT-AC.sh
@@ -11,9 +11,7 @@ git reset --hard
 git checkout mips-RT-AC
 clear
 
-patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src/router/common.mak
 patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Makefile
-patch -i $FT_PATCHES_DIR/mksquashfs.c.patch $FT_REPO_DIR/release/src-rt-6.x/linux/linux-2.6/scripts/squashfs/mksquashfs.c
 patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
 patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src/router/apcupsd/autoconf/configure.in
 patch -i  $FT_PATCHES_DIR/configure.ac_transmission.patch $FT_REPO_DIR/release/src/router/transmission/configure.ac
