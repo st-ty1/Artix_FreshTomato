ac05490a4a491dedbf5aecddaa53edd52a68d70e
st-ty1 <stephan.tratzky@gmail.com>
2021-05-31 19:09:47 +0200

    Update build_ft-arm.sh

--- dockerfiles/build_ft-arm.sh
+++ dockerfiles/build_ft-arm.sh
@@ -13,9 +13,10 @@ git reset --hard
 git checkout arm-master
 
 clear
-patch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/common.mak
 patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
-patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
+patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/miniupnpd/configure
+patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/apcupsd/autoconf/configure.in
+patch -i $FT_PATCHES_DIR/configure.ac_transmission.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/transmission/configure.ac
 
 cd release/src-rt-6.x.4708
 
