0fa5f0be9838b445348a2bff6d544bb0a248efa3
st-ty1 <stephan.tratzky@gmail.com>
2021-11-05 19:44:16 +0100

    Update build_ft-arm_mysql.sh

--- mysql_autotools/build_ft-arm_mysql.sh
+++ mysql_autotools/build_ft-arm_mysql.sh
@@ -3,8 +3,6 @@
 FT_PATCHES_DIR=$HOME/Artix_FreshTomato
 FT_REPO_DIR=$HOME/freshtomato-arm
 
-export PATH="$PATH:$FT_REPO_DIR/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3/bin"
-
 cd $FT_REPO_DIR 
 git clean -dxf
 git reset --hard
@@ -12,16 +10,7 @@ git reset --hard
 
 git checkout arm-master
 
-clear
-
 patch -i $FT_PATCHES_DIR/mysql_autotools/Makefile_autotools.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
-
-## standard ARTIX-patches
-patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
-patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/miniupnpd/configure
-patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/apcupsd/autoconf/configure.in
-patch -i  $FT_PATCHES_DIR/configure.ac_transmission.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/transmission/configure.ac
-
 
 cd $FT_REPO_DIR/release/src-rt-6.x.4708
 
