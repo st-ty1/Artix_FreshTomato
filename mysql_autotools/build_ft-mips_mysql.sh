3fb2edbf40e2b70481766f21ea04d49d38609000
st-ty1 <stephan.tratzky@gmail.com>
2021-11-05 19:42:44 +0100

    Update build_ft-mips_mysql.sh

--- mysql_autotools/build_ft-mips_mysql.sh
+++ mysql_autotools/build_ft-mips_mysql.sh
@@ -9,17 +9,10 @@ git reset --hard
 #git pull
 
 git checkout mips-RT-AC
-clear
 
 patch -i $FT_PATCHES_DIR/mysql_autotools/Makefile_autotools.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 
-## standard ARTIX-patches
-patch -i $FT_PATCHES_DIR/Makefile_mips.patch $FT_REPO_DIR/release/src/router/Makefile
-patch -i $FT_PATCHES_DIR/miniupnpd_config.patch $FT_REPO_DIR/release/src/router/miniupnpd/configure
-patch -i $FT_PATCHES_DIR/configure.in_apcupsd.patch $FT_REPO_DIR/release/src/router/apcupsd/autoconf/configure.in
-patch -i  $FT_PATCHES_DIR/configure.ac_transmission.patch $FT_REPO_DIR/release/src/router/transmission/configure.ac
-
 cd $FT_REPO_DIR/release/src-rt-6.x
-exit
+
 time make wndr4500v2z #1>log.txt 2>&1
 # AIO:z; VPN:e > build.txt
