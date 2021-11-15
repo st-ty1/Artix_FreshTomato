e50410698eec8763b362f7f676f9a79f84cfe3dc
st-ty1 <stephan.tratzky@gmail.com>
2021-11-15 20:56:19 +0100

    Update build_ft-arm_libfoo.sh

--- arm_Samba4/libfoo/build_ft-arm_libfoo.sh
+++ arm_Samba4/libfoo/build_ft-arm_libfoo.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 
-## path to special Artix-patches for building FT
-FT_PATCHES_DIR=$HOME/Artix_FreshTomato
-
 ## path to the local FreshTomato repo
 FT_REPO_DIR=$HOME/freshtomato-arm
 
@@ -17,14 +14,12 @@ git checkout arm-master
 clear
 
 #libfoo_arm
-cp -v $LIBFOO_DIR/0101-Create-short-Makefiles-for-Debian.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/patches/diskdev_cmds-332.25
 cp -v $LIBFOO_DIR/libfoo_arm2.pl $FT_REPO_DIR/release/src-rt-6.x.4708/btools/libfoo.pl
 patch -i $LIBFOO_DIR/Makefile_libfoo.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 patch -i $LIBFOO_DIR/Makefile_lib_so.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 
 cd $FT_REPO_DIR/release/src-rt-6.x.4708
 
-#make clean
 time make ac68z ## AIO: z; VPN: e
 
 ## change ac68e to your Arm-router model if needed
