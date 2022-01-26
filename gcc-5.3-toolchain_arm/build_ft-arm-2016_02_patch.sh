187baf01581e9ce7124fa860a67c81d2b016f56c
st-ty1 <stephan.tratzky@gmail.com>
2022-01-26 13:31:05 +0100

    Update build_ft-arm-2016_02_patch.sh

--- gcc-5.3-toolchain_arm/build_ft-arm-2016_02_patch.sh
+++ gcc-5.3-toolchain_arm/build_ft-arm-2016_02_patch.sh
@@ -27,48 +27,47 @@ mkdir $FT_REPO_DIR/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-
 cp -rvf $FT_TOOLCHAIN_DIR/usr/* $FT_REPO_DIR/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3/usr
 
 ## router/Makefile
-patch -i $FT_PATCHES_DIR/FT/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
+patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 ## router/libbcm
-patch -i $FT_PATCHES_DIR/FT/Makefile_libbcm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/libbcm/Makefile
+patch -i $FT_PATCHES_DIR/Makefile_libbcm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/libbcm/Makefile
 ## router/rc
-patch -i $FT_PATCHES_DIR/FT/services.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/rc/services.c
-
+patch -i $FT_PATCHES_DIR/services.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/rc/services.c
 ## router/shared
-patch -i $FT_PATCHES_DIR/FT/shutils.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/shared/shutils.h
+patch -i $FT_PATCHES_DIR/shutils.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/shared/shutils.h
 ## router/ebtables
-patch -i $FT_PATCHES_DIR/FT/libebtc.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/ebtables/libebtc.c
+patch -i $FT_PATCHES_DIR/libebtc.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/ebtables/libebtc.c
 ## router/httpd
-patch -i $FT_PATCHES_DIR/FT/ctype.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3/usr/arm-brcm-linux-uclibcgnueabi/sysroot/usr/include/ctype.h
-patch -i $FT_PATCHES_DIR/FT/Makefile_httpd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/httpd/Makefile
+patch -i $FT_PATCHES_DIR/ctype.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3/usr/arm-brcm-linux-uclibcgnueabi/sysroot/usr/include/ctype.h
+patch -i $FT_PATCHES_DIR/Makefile_httpd.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/httpd/Makefile
 ## router/hotplug2
-patch -i $FT_PATCHES_DIR/FT/mem_utils.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/hotplug2/mem_utils.c
-patch -i $FT_PATCHES_DIR/FT/hotplug2_utils.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/hotplug2/hotplug2_utils.c
+patch -i $FT_PATCHES_DIR/mem_utils.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/hotplug2/mem_utils.c
+patch -i $FT_PATCHES_DIR/hotplug2_utils.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/hotplug2/hotplug2_utils.c
 ## router/fmpeg - avoid implicit declarations
-patch -i $FT_PATCHES_DIR/FT/h264dsp_init_arm.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/ffmpeg/libavcodec/arm/h264dsp_init_arm.c
-patch -i $FT_PATCHES_DIR/FT/h264pred_init_arm.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/ffmpeg/libavcodec/arm/h264pred_init_arm.c
+patch -i $FT_PATCHES_DIR/h264dsp_init_arm.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/ffmpeg/libavcodec/arm/h264dsp_init_arm.c
+patch -i $FT_PATCHES_DIR/h264pred_init_arm.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/ffmpeg/libavcodec/arm/h264pred_init_arm.c
 ## router/wireguard - definition from Linux 3.1 netlink.h
-patch -i $FT_PATCHES_DIR/FT/netlink_wireguard.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/wireguard-tools/src/netlink.h
+patch -i $FT_PATCHES_DIR/netlink_wireguard.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/wireguard-tools/src/netlink.h
 ## router/dnscrypt - due to message "using unsafe headers"
-patch -i $FT_PATCHES_DIR/FT/configure.ac_dnscrypt.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/dnscrypt/configure.ac
+patch -i $FT_PATCHES_DIR/configure.ac_dnscrypt.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/dnscrypt/configure.ac
 # router/glib multiple definitions - alternate 1
-patch -i $FT_PATCHES_DIR/FT/glib.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/glib/glib.h
+patch -i $FT_PATCHES_DIR/glib.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/glib/glib.h
 # router/glib multiple definitions - alternate 2
-#patch -i $FT_PATCHES_DIR/FT/glib_mod1.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/glib/glib.h
-#patch -i $FT_PATCHES_DIR/FT/gmessages.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/glib/gmessages.c
+#patch -i $FT_PATCHES_DIR/glib_mod1.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/glib/glib.h
+#patch -i $FT_PATCHES_DIR/gmessages.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/glib/gmessages.c
 
 ## kernel
-patch -p1 -d$FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36 < $FT_PATCHES_DIR/FT/linux-2.6.32.60-gcc5.patch
+patch -p1 -d$FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36 < $FT_PATCHES_DIR/linux-2.6.32.60-gcc5.patch
 ## module: et-driver
-patch -i $FT_PATCHES_DIR/FT/et_linux.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/et_linux.c
-patch -i $FT_PATCHES_DIR/FT/etc.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc.c
-patch -i $FT_PATCHES_DIR/FT/etc_adm.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc_adm.c
-patch -i $FT_PATCHES_DIR/FT/etc47xx.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc47xx.c
-patch -i $FT_PATCHES_DIR/FT/etcgmac.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etcgmac.c
-patch -i $FT_PATCHES_DIR/FT/etc_fa.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc_fa.c
+patch -i $FT_PATCHES_DIR/et_linux.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/et_linux.c
+patch -i $FT_PATCHES_DIR/etc.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc.c
+patch -i $FT_PATCHES_DIR/etc_adm.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc_adm.c
+patch -i $FT_PATCHES_DIR/etc47xx.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc47xx.c
+patch -i $FT_PATCHES_DIR/etcgmac.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etcgmac.c
+patch -i $FT_PATCHES_DIR/etc_fa.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/et/sys/etc_fa.c
 ## module: 4g modem driver cdc-ncm.c
-patch -i $FT_PATCHES_DIR/FT/string.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/string.h
-patch -i $FT_PATCHES_DIR/FT/kernel.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/kernel.h
-patch -i $FT_PATCHES_DIR/FT/string.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36/lib/string.c
+patch -i $FT_PATCHES_DIR/string.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/string.h
+patch -i $FT_PATCHES_DIR/kernel.h.patch $FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/kernel.h
+patch -i $FT_PATCHES_DIR/string.c.patch $FT_REPO_DIR/release/src-rt-6.x.4708/linux/linux-2.6.36/lib/string.c
 
 cd $FT_REPO_DIR/release/src-rt-6.x.4708
 
