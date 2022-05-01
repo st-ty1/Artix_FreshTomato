03de3de1998cb71ad0da761a5d287237bab9dc37
Stephan Tratzky <tratzky@freenet.de>
2022-05-01 13:24:54 +0200

    narrow down Makefile.patch and update arm build scripts

--- build_ft-arm7.sh
+++ build_ft-arm7.sh
@@ -12,7 +12,7 @@ git checkout arm-sdk7
 
 clear
 
-patch -i $FT_PATCHES_DIR/Makefile_arm.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
+patch -i $FT_PATCHES_DIR/Makefile.patch $FT_REPO_DIR/release/src-rt-6.x.4708/router/Makefile
 
 cd release/src-rt-7.x.main/src
 
