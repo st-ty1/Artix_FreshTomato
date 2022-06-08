9e7f4a1f82709c029166469334154912ee305be6
st-ty1 <stephan.tratzky@gmail.com>
2022-06-08 11:21:32 +0200

    Update build_ft-mips-2016_02_RT-AC.sh
    Makefile patch of router/httpd obsolete

--- gcc-7.3-toolchain_mips-ng/build_ft-mips-2016_02_RT-AC.sh
+++ gcc-7.3-toolchain_mips-ng/build_ft-mips-2016_02_RT-AC.sh
@@ -51,7 +51,6 @@ patch -i $FT_PATCHES_DIR/libebtc.c.patch $FT_REPO_DIR/release/src/router/ebtable
 
 ## router/httpd
 patch -i $FT_PATCHES_DIR/ctype.h.patch $FT_REPO_DIR/tools/brcm/K26/hndtools-mipsel-uclibc-7.3-ng/usr/mipsel-brcm-linux-uclibc/sysroot/usr/include/ctype.h
-patch -i $FT_PATCHES_DIR/Makefile_httpd.patch $FT_REPO_DIR/release/src/router/httpd/Makefile
 
 ## router/hotplug2; 3rd patch new for gcc-7.3
 patch -i $FT_PATCHES_DIR/mem_utils.c.patch $FT_REPO_DIR/release/src/router/hotplug2/mem_utils.c
