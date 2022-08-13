2a5d0dbe6714386d668354c951294cfb2d1ce29b
st-ty1 <stephan.tratzky@gmail.com>
2022-08-13 15:52:35 +0000

    removed files not needed

--- build_ft-mips-RT-AC.sh
+++ build_ft-mips-RT-AC.sh
@@ -14,7 +14,12 @@ clear
 
 patch -i $FT_PATCHES_DIR/Makefile.patch $FT_REPO_DIR/release/src/router/Makefile
 
+############# only needed on Artix, when having complete graphical desktop environment ##############
+cp -vf $FT_PATCHES_DIR/CMakeLists.txt $FT_REPO_DIR/release/src/router/getdns
+rm $FT_REPO_DIR/release/src/router/nettle/desdata.stamp
+patch -i  $FT_PATCHES_DIR/Makefile_transmission.patch $FT_REPO_DIR/release/src/router/Makefile
+##############################################################################
 cd release/src-rt-6.x
 
-time make r64z #1> log.txt 2>&1
+time make r64e #1> log.txt 2>&1
 ## Insert you router model in line above; AIO:z; VPN:e
