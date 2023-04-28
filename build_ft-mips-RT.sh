6b7e4fafc4c598732c493fb2cba2a50e729672ad
st-ty1 <stephan.tratzky@gmail.com>
2023-04-28 11:12:38 +0200

    Update build_ft-mips-RT.sh

--- build_ft-mips-RT.sh
+++ build_ft-mips-RT.sh
@@ -16,5 +16,5 @@ patch -i $FT_PATCHES_DIR/Makefile.patch $FT_REPO_DIR/release/src/router/Makefile
 
 cd release/src-rt
 
-time make r64e #1> log.txt 2>&1
+time make r2z #1> log.txt 2>&1
 ## AIO:z; VPN:e
