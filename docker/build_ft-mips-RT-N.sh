f271bef268f672b5699ed17e34e8a715e8e02121
st-ty1 <stephan.tratzky@gmail.com>
2024-10-13 16:59:42 +0200

    rearranged toolchains

---
+++ docker/build_ft-mips-RT-N.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+FT_PATCHES_DIR=$HOME/Artix_FreshTomato
+FT_REPO_DIR=$HOME/freshtomato-mips
+
+cd $FT_REPO_DIR 
+git clean -dxf 
+git reset --hard
+git pull
+
+git checkout mips-RT-AC
+
+clear
+
+patch -i $FT_PATCHES_DIR/Makefile.patch $FT_REPO_DIR/release/src/router/Makefile
+
+cd release/src-rt
+
+make $@
+
+mkdir /image
+cp $FT_REPO_DIR/release/src-rt/image/* /image
