f41658e192f86f1d5fdee4cffc8d7e36fc00e01a
st-ty1 <stephan.tratzky@gmail.com>
2024-10-17 19:41:52 +0200

    AX update

--- AX/build_ft-arm-ax.sh
+++ AX/build_ft-arm-ax.sh
@@ -6,7 +6,7 @@ FT_REPO_DIR=$HOME/freshtomato-ax
 cd $FT_REPO_DIR
 git clean -dxf 
 git reset --hard
-git pull
+#git pull
 
 git checkout master
 
@@ -18,7 +18,7 @@ sudo ln -s $FT_REPO_DIR/release/src-rt-5.04axhnd.675x/toolchains/brcm /opt/toolc
 # according to Readme of repo of freshtomato-ax
 export PATH=$PATH:/opt/toolchains/crosstools-aarch64-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32/usr/bin
 export PATH=$PATH:/opt/toolchains/crosstools-arm-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32/usr/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/toolchains/crosstools-arm-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32/usr/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/toolchains/crosstools-arm-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32/usr/lib
 
 # Due to setting of LD_LIBRARY_PATH, pkgconf of host-OS Artix Linux is forced to use lib of pkconf, packed with toolchains of freshtomato-ax.
 # But both versions seem to differ either in their version number or in way they were build, therefore resulting in error "missing symbols". 
@@ -31,7 +31,12 @@ rm -v $FT_REPO_DIR/release/src-rt-5.04axhnd.675x/toolchains/brcm/crosstools-aarc
 patch -i  $FT_PATCHES_DIR/AX/Makefile_ax.patch $FT_REPO_DIR/release/src/router/Makefile
 
 # intermediate patch, only needed with actual iperf3 version; should be obsolete when using higher versions of iperf3 in future
-patch -i  $FT_PATCHES_DIR/AX/configure.ac_iperf3.patch $FT_REPO_DIR/release/src/router/iperf3/configure.ac
+patch -i  $FT_PATCHES_DIR/AX/iperf3_configure.ac.patch $FT_REPO_DIR/release/src/router/iperf3/configure.ac
+
+# new: curl
+patch -i  $FT_PATCHES_DIR/AX/curl_configure.ac.patch $FT_REPO_DIR/release/src/router/curl/configure.ac
+
+patch -i  $FT_PATCHES_DIR/AX/config_Makefile.patch $FT_REPO_DIR/release/src/router/config/Makefile
 
 cd $HOME/freshtomato-ax/release/src-rt-5.04axhnd.675x
-make tuf-ax3000_v2 1> log.txt 2>&1  #1>log.txt
+make tuf-ax3000_v2 #1> log.txt 2>&1  #1>log.txt
